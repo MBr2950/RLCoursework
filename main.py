be0b2fc3ac6f4a98ea0b9ce8829a0160fa828127
Max Bridport <mb2950@bath.ac.uk>
2023-12-12 16:08:05 +0000

    Changed basic code in main.py, now using gymnasium tutorial as base. Current code is using the REINFORCE algorithm, this class has to be replaced by A3C. Using InvertedPendulum for now, much easier for initial training.

--- main.py
+++ main.py
@@ -1,204 +1,238 @@
-#  basic pseudocode (as guide, won't be efficient):
-#  https://moodle.bath.ac.uk/mod/page/view.php?id=1146237&forceview=1
+# Basic code largely adapted from the following tutorial:
+# https://gymnasium.farama.org/tutorials/training_agents/reinforce_invpend_gym_v26/
 
-#  pseudocode:
-#  https://moodle.bath.ac.uk/mod/page/view.php?id=1146241
-
-import gymnasium as gym
-import torch
-from torch import nn
-import random
+import random, cv2
+import matplotlib.pyplot as plt
 import numpy as np
-env = gym.make('Humanoid-v4',  exclude_current_positions_from_observation=False)
-observation, info = env.reset()
-
-## The Neural Networks
-class ActorNetwork(torch.nn.Module):
-
-
-    def __init__(self):
-        #initialises the network
-        #we need to decide what our network will look like
-        super(ActorNetwork, self).__init__()
-        self.NN = nn.Sequential(
-            nn.Linear(378, 500),
-            nn.ReLU(),
-            nn.Linear(500, 100),
-            nn.ReLU(),
-            nn.Linear(100, 32),
-            nn.ReLU(),
-            nn.Linear(32, 17))
-        
-        self.optimizer = torch.optim.SGD(self.parameters(), lr=0.01, momentum=0.5)
+import pandas as pd
+import seaborn as sns
+import torch
+import torch.nn as nn
+from torch.distributions.normal import Normal
+import gymnasium as gym
 
 
-    def ChooseAction(self, State):
-        #uses the NN to estimate an action for a given state
-        State = torch.from_numpy(State).float()
-        return (self.NN(State)).detach().numpy()# converts from np to tensor, then back
-    
-        #TODO: Add randomisation
-    
-    def Update(self, values):
-        #trains the network
-        values = values.detach().numpy()
+
+# Neural network for giving actions
+class ActorNetwork(nn.Module):
+    # observationDim, actionDim are integers defining the number of observations 
+    # and actions available in the given model
+    def __init__(self, observationDim, actionDim) -> None:
+        super().__init__()
+
+        # Define neural network properties
+        self.sharedNet = nn.Sequential(
+                nn.Linear(observationDim, 500),
+                nn.ReLU(),
+                nn.Linear(500, 100),
+                nn.ReLU(),
+                nn.Linear(100, 32),
+                nn.ReLU(),
+        )
+
+        # Policy Mean specific Linear Layer
+        self.policyMeanNet = nn.Sequential(
+            nn.Linear(32, actionDim)
+        )
+
+        # Policy Std Dev specific Linear Layer
+        self.policyStdDevNet = nn.Sequential(
+            nn.Linear(32, actionDim)
+        )
+
+
+    # Performs one forward pass on the neural network
+    # Takes in a tensor 'x'
+    # Returns two tensors, 'actionMeans' and 'actionStdDevs', both predicted
+    # from the normal distribution
+    def forward(self, x):
+        sharedFeatures = self.sharedNet(x.float())
+
+        actionMeans = self.policyMeanNet(sharedFeatures)
+        actionStdDevs = torch.log(
+            1 + torch.exp(self.policyStdDevNet(sharedFeatures))
+        )
+
+        return actionMeans, actionStdDevs
+
+
+class REINFORCE:
+    """REINFORCE algorithm."""
+
+    def __init__(self, obs_space_dims: int, action_space_dims: int):
+        """Initializes an agent that learns a policy via REINFORCE algorithm [1]
+        to solve the task at hand (Inverted Pendulum v4).
+
+        Args:
+            obs_space_dims: Dimension of the observation space
+            action_space_dims: Dimension of the action space
+        """
+
+        # Hyperparameters
+        self.learning_rate = 1e-4  # Learning rate for policy optimization
+        self.gamma = 0.99  # Discount factor
+        self.eps = 1e-6  # small number for mathematical stability
+
+        self.probs = []  # Stores probability values of the sampled action
+        self.rewards = []  # Stores the corresponding rewards
+
+        self.net = ActorNetwork(obs_space_dims, action_space_dims)
+        self.optimizer = torch.optim.AdamW(self.net.parameters(), lr=self.learning_rate)
+
+    def sample_action(self, state: np.ndarray) -> float:
+        """Returns an action, conditioned on the policy and observation.
+
+        Args:
+            state: Observation from the environment
+
+        Returns:
+            action: Action to be performed
+        """
+        state = torch.tensor(np.array([state]))
+        action_means, action_stddevs = self.net(state)
+
+        # create a normal distribution from the predicted
+        #   mean and standard deviation and sample an action
+        distrib = Normal(action_means[0] + self.eps, action_stddevs[0] + self.eps)
+        action = distrib.sample()
+        prob = distrib.log_prob(action)
+
+        action = action.numpy()
+
+        self.probs.append(prob)
+
+        return action
+
+    def update(self):
+        """Updates the policy network's weights."""
+        running_g = 0
+        gs = []
+
+        # Discounted return (backwards) - [::-1] will return an array in reverse
+        for R in self.rewards[::-1]:
+            running_g = R + self.gamma * running_g
+            gs.insert(0, running_g)
+
+        deltas = torch.tensor(gs)
+
+        loss = 0
+        # minimize -1 * prob * reward obtained
+        for log_prob, delta in zip(self.probs, deltas):
+            loss += log_prob.mean() * delta * (-1)
+
+        # Update the policy network
         self.optimizer.zero_grad()
-        E = 0
-        for i in range(len(values)):
-            E += (i + 1) * values[i]
-        E1 = torch.tensor(E, requires_grad=True)
-        E1.backward()
-        E1 = -E1
-        
-        self.optimizer.step()
-        
-
-    def Refresh(self, pi):
-        #Updates pi2 to match pi1
-        for param1, param2 in zip(self.parameters(), pi.parameters()):
-            param1 = Beta * param1.data + (1-Beta) * param2.data
-
-       #Inspired by:
-       #https://github.com/DLR-RM/stable-baselines3/issues/93
-       #May need to cite
-
-         
-
-class CriticNetwork(torch.nn.Module):
-
-    def __init__(self):
-        #initialises the network
-        super(CriticNetwork, self).__init__()
-        self.NN = nn.Sequential(
-            nn.Linear(395, 900),
-            nn.ReLU(),
-            nn.Linear(900, 300),
-            nn.ReLU(),
-            nn.Linear(300, 90),
-            nn.ReLU(),
-            nn.Linear(90, 30),
-            nn.ReLU(),
-            nn.Linear(30, 10),
-            nn.ReLU(),
-            nn.Linear(10, 1))
-        
-        self.optimizer = torch.optim.SGD(self.parameters(), lr=0.01, momentum=0.5)
-    
-    def ActionValue(self, State, Action):
-        #uses the NN to estimate the action-value for a given state and action
-        return self.NN(torch.from_numpy(np.concatenate((State, Action))).float())
-    
-    def Update(self, y1, y2):
-        #trains the network
-        self.optimizer.zero_grad()
-        loss = nn.functional.huber_loss(y1, y2)
         loss.backward()
         self.optimizer.step()
+
+        # Empty / zero out all episode-centric/related variables
+        self.probs = []
+        self.rewards = []
+
+
+
+# Create and wrap the environment
+env = gym.make("InvertedPendulum-v4")
+wrappedEnv = gym.wrappers.RecordEpisodeStatistics(env, 50)  # Records episode-reward
+
+numEpisodes = int(1e3)  # Total number of episodes
+observationDims = env.observation_space.shape[0]
+actionDims = env.action_space.shape[0]
+seedRewards = []
+
+for seed in [1, 2, 3, 5, 8]:  # Fibonacci seeds
+    # set seed
+    torch.manual_seed(seed)
+    random.seed(seed)
+    np.random.seed(seed)
+
+    # Reinitialize agent every seed
+    agent = REINFORCE(observationDims, actionDims)
+    reward_over_episodes = []
+
+    for episode in range(numEpisodes):
+        # gymnasium v26 requires users to set seed while resetting the environment
+        obs, info = wrappedEnv.reset(seed=seed)
+
+        done = False
+        while not done:
+            action = agent.sample_action(obs)
+
+            # Step return type - `tuple[ObsType, SupportsFloat, bool, bool, dict[str, Any]]`
+            # These represent the next observation, the reward from the step,
+            # if the episode is terminated, if the episode is truncated and
+            # additional info from the step
+            obs, reward, terminated, truncated, info = wrappedEnv.step(action)
+            agent.rewards.append(reward)
+
+            # End the episode when either truncated or terminated is true
+            #  - truncated: The episode duration reaches max number of timesteps
+            #  - terminated: Any of the state space values is no longer finite.
+            done = terminated or truncated
+
+        reward_over_episodes.append(wrappedEnv.return_queue[-1])
+        agent.update()
+
+        if episode % 100 == 0:
+            avg_reward = int(np.mean(wrappedEnv.return_queue))
+            print("Episode:", episode, "Average Reward:", avg_reward)
+
+    seedRewards.append(reward_over_episodes)
+    
+
+env = gym.make("InvertedPendulum-v4", render_mode="human")
+wrappedEnv = gym.wrappers.RecordEpisodeStatistics(env, 50)  # Records episode-reward
+
+numEpisodes = int(1e3)  # Total number of episodes
+
+for seed in [1, 2, 3, 5, 8]:  # Fibonacci seeds
+    # set seed
+    torch.manual_seed(seed)
+    random.seed(seed)
+    np.random.seed(seed)
+
+    # Reinitialize agent every seed
+
+    for episode in range(numEpisodes):
+        # gymnasium v26 requires users to set seed while resetting the environment
+        obs, info = wrappedEnv.reset(seed=seed)
+
+        done = False
+        while not done:
+            action = agent.sample_action(obs)
+
+            # Step return type - `tuple[ObsType, SupportsFloat, bool, bool, dict[str, Any]]`
+            # These represent the next observation, the reward from the step,
+            # if the episode is terminated, if the episode is truncated and
+            # additional info from the step
+            obs, reward, terminated, truncated, info = wrappedEnv.step(action)
+            agent.rewards.append(reward)
+
+            # End the episode when either truncated or terminated is true
+            #  - truncated: The episode duration reaches max number of timesteps
+            #  - terminated: Any of the state space values is no longer finite.
+            done = terminated or truncated
+
+        reward_over_episodes.append(wrappedEnv.return_queue[-1])
+        agent.update()
+
+        if episode % 100 == 0:
+            avg_reward = int(np.mean(wrappedEnv.return_queue))
+            print("Episode:", episode, "Average Reward:", avg_reward)
         
-    def Refresh(self, q):
-            #Updates q2 to match q1
-            for param1, param2 in zip(self.parameters(), q.parameters()):
-                param1 = Beta * param1.data + (1-Beta) * param2.data
-    
+            # use mode 'rgb_array'
+            # img = cv2.cvtColor(env.render(), cv2.COLOR_RGB2BGR)
+            # cv2.imshow("test", img)
+            # cv2.waitKey(50)
 
-def DataFunction(reward):
-    print("Current reward:")
-    print("healthy_reward + forward_reward - ctrl_cost - contact_cost: " + str(reward))
-    print("https://gymnasium.farama.org/environments/mujoco/humanoid/#rewards")
-    
-D = list()#todo: initialise this list by allowing agent to wander randomly for some time steps 
-observation, info = env.reset()
-for i in range(1000):
-    action = env.action_space.sample() #pick random action
-    state = observation
-    observation, reward, terminated, truncuated, info = env.step(action)
-    state = state.astype(float)
-    action = action.astype(float)
-    observation = observation.astype(float)
-    D.append([state, action, reward, observation])
-
-pi1 = ActorNetwork()
-pi2 = ActorNetwork()
-q1 = CriticNetwork()
-q2 = CriticNetwork()
+    seedRewards.append(reward_over_episodes)
 
 
-
-Beta = 0.1 #Incremental refreshing rate
-minibatch = 5 #Taken from D
-gamma = 0.1 #learning rate
-dataprint = 0
-
-#Main loop, i is number of episodes
-for i in range(1000):
-    observation, info = env.reset()
-    #an episode
-    rewardlist = list()
-
-    while (True):
-        
-        #chooses action, notes new information
-        action = pi1.ChooseAction(observation)
-
-
-        for j in action:
-            j = j + np.random.normal(0, 0.1)#adds randomisation to action for all 16 limbs,+/- 0.1 max, caps at -0.4 and 0.4
-            if j > 0.4:
-                j = 0.4
-            if j < -0.4:
-                j = -0.4
-
-        state = observation
-        observation, reward, terminated, truncuated, info = env.step(action)
-        state = state.astype(float)
-        action = action.astype(float)
-        observation = observation.astype(float)
-        D.append([state, action, reward, observation])
-
-        rewardlist.append(reward)
-        #dataprint += 1
-        #if dataprint == 1000:
-        #    dataprint = 0
-        #    DataFunction(reward)
-
-
-        #updates Action-Value estimate (NN)
-        for j in range(minibatch):
-            transition = D[random.randint(0, len(D) - 1)]
-            # Picks a random sample from D 
-            #0-State, 1- Action, 2- Reward, 3- Observation
-            q1y = transition[2] + gamma * q2.ActionValue(transition[3], pi2.ChooseAction(transition[3]))
-            q1.Update(q1y, q1.ActionValue(transition[0], transition[1]))
-
-            pi1.Update(q1.ActionValue(transition[0], pi1.ChooseAction(transition[0])))
-        
-
-        #update the beta thing instead of C here
-        pi2.Refresh(pi1)
-        q2.Refresh(q1)
-
-        #ends the episode
-        if terminated or truncuated:
-            observation, info = env.reset()
-            break
-    
-    totreward = 0
-    for rew in rewardlist:
-        totreward += rew
-    print("Episode: " + str(i) + ". Reward Sum = " + str(totreward))
-
-env.close()
-
-
-
-
-
-
-
-    
-       
-
-
-    
-     
-
+rewards_to_plot = [[reward[0] for reward in rewards] for rewards in seedRewards]
+df1 = pd.DataFrame(rewards_to_plot).melt()
+df1.rename(columns={"variable": "episodes", "value": "reward"}, inplace=True)
+sns.set(style="darkgrid", context="talk", palette="rainbow")
+sns.lineplot(x="episodes", y="reward", data=df1).set(
+    title="REINFORCE for InvertedPendulum-v4"
+)
+plt.show()
