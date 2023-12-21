b235944cced5798b955b4808323c9b2b9d756a8f
Max Bridport <max.bridport@hotmail.co.uk>
2023-12-21 22:14:19 +0000

    Added entropy regularisation. GAE was also added, but seemed to worsen performance, was left in commented. Model now produces good results, but is very unstable, and can change drastically based on seed used.

--- A2C.py
+++ A2C.py
@@ -42,19 +42,15 @@ class ActorCritic(nn.Module):
             torch.nn.Linear(32, outputDims)
         )
 
-        self.criticValue = torch.nn.Sequential(*criticLayers)
+        self.critic = torch.nn.Sequential(*criticLayers)
 
         # Avoids type issues
         self.double()
 
     
     def forward(self, state):
-        # x contains the actual criticValues at nodes
-        # Sometimes input is array, sometimes tensor?
-        if type(state) == np.ndarray:
-            x = self.model(torch.tensor(state))
-        else:
-            x = self.model(torch.tensor(state[0]))
+        # x contains the actual values at nodes
+        x = self.model(torch.tensor(state))
 
         # Mean and standard deviation of predicted best action are
         #  returned, to allow for sampling from normal distribution
@@ -63,12 +59,9 @@ class ActorCritic(nn.Module):
             1 + torch.exp(self.modelStdDev(x))
         )
 
-        if type(state) == np.ndarray:
-            criticValue = self.criticValue(torch.tensor(state))
-        else:
-            criticValue = self.criticValue(torch.tensor(state[0]))
+        Qvalue = self.critic(torch.tensor(state))
 
-        return means, stdDevs, criticValue
+        return means, stdDevs, Qvalue
 
 
 
@@ -80,48 +73,74 @@ class A2C():
         self.actorCritic = ActorCritic(self.inputDims, self.outputDims)
         self.actorCriticOptimiser = optim.Adam(self.actorCritic.parameters(), lr=0.0003)
 
-        self.allLengths = []
-        self.averageLengths = []
         self.allRewards = []
-        self.entropyTerm = 0
+        self.averageRewards = []
 
+        # Discount value
         self.gamma = 0.99
-        self.numSteps = 300
+        # For calculating GAE
+        # self.lambdaValue = 0.95
+        self.maxNumSteps = 1000
+        # This is n step A2C
+        self.nSteps = 20
         self.maxEpisodes = 3000
 
     def chooseAction(self, means, stdDevs):
             # Defines a distibution for each action, samples from it, then finds the 
             # probability of taking that action, for calculating loss
-            actions = []
-            distributions = []
-            # Probability of all actions being taken together
-            fullProbability = 0
 
-            for i in range(self.outputDims):
-                distributions.append(torch.distributions.Normal(means[i], stdDevs[i]))
-                actions.append(distributions[i].sample())
-                fullProbability += distributions[i].log_prob(actions[i])
-            fullProbability = fullProbability / self.outputDims
+            # If outputDims = 1, means[0] changes meaning from first action to first element in action
+            if self.outputDims == 1:
+                distribution = torch.distributions.Normal(means, stdDevs)
+                action = distribution.sample()
+                probability = distribution.log_prob(action)
+                entropy = distribution.entropy()
 
-            # print(actions)
+                return action, probability, entropy
+            else:
+                actions = []
+                distributions = []
+                # Probability of all actions being taken together
+                fullProbability = 0
+                entropy = 0
 
-            return actions, fullProbability
+                for i in range(self.outputDims):
+                    distributions.append(torch.distributions.Normal(means[i], stdDevs[i]))
+                    actions.append(distributions[i].sample())
+                    fullProbability += distributions[i].log_prob(actions[i])
+                    entropy += distributions[i].entropy()
+                fullProbability = fullProbability / self.outputDims
+                entropy = entropy / self.outputDims
+
+                return actions, fullProbability, entropy
 
 
-    def updateNetwork(self, criticValues, QValues, probabilities):
+    def updateNetwork(self, QValues, rewards, probabilities, entropies, terminateds):
+        # compute returns
+        returns = np.zeros_like(QValues)
+        myReturn = 0
+        for t in reversed(range(len(rewards))):
+            myReturn = rewards[t] + self.gamma * myReturn
+            returns[t] = myReturn
+
         #update actor critic
-        criticValues = torch.FloatTensor(np.array(criticValues))
-        QValues = torch.FloatTensor(QValues)
+        QValues = torch.FloatTensor(np.array(QValues))
+        returns = torch.FloatTensor(returns)
         probabilities = torch.stack(probabilities)
+        entropies = torch.tensor(entropies)
             
-        advantage = QValues - criticValues
-        actorLoss = (-probabilities * advantage).mean()
-        criticLoss = (0.5 * advantage.pow(2)).mean()
+        advantages = returns - QValues
+        # advantages = self.calculateGAE(rewards, QValues, terminateds)
+
+        # print(probabilities, advantages)  
+        actorLoss = (-probabilities * advantages).mean()
+        criticLoss = (0.5 * advantages.pow(2)).mean()
+        entropy = entropies.mean()
 
         # print("\n actorLoss: ", actorLoss, "\n criticLoss: ", criticLoss, "\n entropyTerm: ", self.entropyTerm)
-        # print("\n criticValues: ", criticValues, "\n QValues: ", QValues)
+        # print("\n QValues: ", QValues, "\n returns: ", returns)
 
-        actorCriticLoss = actorLoss + criticLoss #+ 0.001 * self.entropyTerm
+        actorCriticLoss = actorLoss + criticLoss + 0.001 * entropy
 
         self.actorCriticOptimiser.zero_grad()
         actorCriticLoss.backward()
@@ -131,45 +150,53 @@ class A2C():
     def main(self, env):
         for episode in range(self.maxEpisodes):
             probabilities = []
-            criticValues = []
+            entropies = []
+            QValues = []
             rewards = []
+            terminateds = []
 
-            state = env.reset()
-            for steps in range(self.numSteps):
-                means, stdDevs, criticValue = self.actorCritic.forward(state)
-                criticValue = criticValue.detach().numpy()
-
-                action, probability = self.chooseAction(means, stdDevs)
-
-                entropy = -(action * np.log(action))
+            state, info = env.reset()
+            for steps in range(self.maxNumSteps):
+                means, stdDevs, Qvalue = self.actorCritic.forward(state)
+                Qvalue = Qvalue.detach().numpy()
+                action, probability, entropy = self.chooseAction(means, stdDevs)
                 newState, reward, terminated, truncated, info = env.step(action)
 
                 rewards.append(reward)
-                criticValues.append(criticValue)
+                QValues.append(Qvalue)
                 probabilities.append(probability)
-                self.entropyTerm += entropy
+                entropies.append(entropy)
+                terminateds.append(terminated)
                 state = newState
                 
-                if terminated or steps == self.numSteps - 1:
+                if terminated or steps == self.maxNumSteps - 1:
                     means, stdDevs, QValue = self.actorCritic.forward(newState)
                     QValue = QValue.detach().numpy()
                     self.allRewards.append(np.sum(rewards))
-                    self.allLengths.append(steps)
-                    self.averageLengths.append(np.mean(self.allLengths[-10:]))
+                    self.averageRewards.append(np.mean(self.allRewards[-10:]))
                     if episode % 100 == 0:                    
-                        print("episode: {}, reward: {}, total length: {}, average length: {}".format(episode, np.sum(rewards), steps, self.averageLengths[-1]))
+                        print("episode: {}, reward: {}, average reward: {}".format(episode, np.sum(rewards), self.averageRewards[-1]))
+                    
+                    # self.updateNetwork(QValues, rewards, probabilities)
                     break
-            
-            # compute Qvalues
-            QValues = np.zeros_like(criticValues)
-            for t in reversed(range(len(rewards))):
-                QValue = rewards[t] + self.gamma * QValue
-                QValues[t] = QValue
-
-            self.updateNetwork(criticValues, QValues, probabilities)
+                
+                # if steps % self.nSteps == 0:
+                #     self.updateNetwork(QValues, rewards, probabilities)
+            self.updateNetwork(QValues, rewards, probabilities, entropies, terminateds)
 
         self.plot()
+
+
+    # def calculateGAE(self, rewards, QValues, terminateds):
+    #     T = len(rewards) - 1
+    #     gaes = [0] * T
+    #     future_gae = 0
+    #     for t in reversed(range(T)):
+    #         delta = rewards[t] + self.gamma * QValues[t + 1] * (not terminateds[t]) - QValues[t]
+    #         gaes[t] = future_gae = delta + self.gamma * self.lambdaValue * (not terminateds[t]) * future_gae
         
+    #     return torch.tensor(gaes)
+
     
     # Plot results
     def plot(self):
@@ -182,17 +209,11 @@ class A2C():
         matplotlib.pyplot.ylabel('Reward')
         matplotlib.pyplot.show()
 
-        matplotlib.pyplot.plot(self.allLengths)
-        matplotlib.pyplot.plot(self.averageLengths)
-        matplotlib.pyplot.xlabel('Episode')
-        matplotlib.pyplot.ylabel('Episode length')
-        matplotlib.pyplot.show()
-
 
 if __name__ == "__main__":
-    env = gym.make("Hopper-v4")
+    env = gym.make("InvertedPendulum-v4")
     a2c = A2C(env)
     a2c.main(env)
 
-    env = gym.make("Hopper-v4", render_mode = "human")
+    env = gym.make("InvertedPendulum-v4", render_mode = "human")
     a2c.main(env)
